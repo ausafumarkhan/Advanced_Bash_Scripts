f48b4d00d2c195ad9d475e102fb15f5fb8ce315a
ausafumarkhan <ausafumarkhan@gmail.com>
2022-05-25 05:52:59 +0530

    Script to take backup using command line argument

--- backup.sh
+++ backup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 BackUpFile=script_backup.tar.gz
-
+echo "####################################################"
 echo "Taking a bakup of $1"
 echo
+echo "####################################################"
 echo "Archiving of $1"
 tar -czvf $BackUpFile $1
+echo "####################################################"
 echo "Moving $BackUpFile to $2"
 echo
 echo "Backup Completed Successfull"
+echo "####################################################"
