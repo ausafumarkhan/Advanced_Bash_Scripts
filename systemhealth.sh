eaa37e1f8df5655dc99fa246d0564b783d4195b7
root <root@localhost.localdomain>
2022-05-25 01:01:33 +0000

    Script to evalute system health

--- systemhealth.sh
+++ systemhealth.sh
@@ -2,7 +2,7 @@
 
 FreeMem=$(free -m|grep Mem|awk '{print $4}')
 LoadAvg=`uptime|awk '{print $9}'`
-RootFree=$(df -h|grep "/dev/mapper/centos_centos7-root"|awk '{print $4}')
+RootFree=$(df -h|grep "/dev/sda1"|awk '{print $4}')
 echo "###########################################################"
 echo
 echo "Available RAM memory: $FreeMem MB"
