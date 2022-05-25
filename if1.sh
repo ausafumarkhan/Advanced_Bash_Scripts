0d5cde4097047900c59468ba0beac41dc49d76fe
root <root@localhost.localdomain>
2022-05-25 00:55:29 +0000

    Script to take backup using decision

--- if1.sh
+++ if1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [[-n $1]] && [[-d $1]]
+if [[ -n $1 ]] && [[ -d $1 ]]
 then
 	echo "You have entered dir path, taking a backup"
 	tar -zcvf backupdir.tar.gz $1
-elif [[-n $1]] && [[-e $1]]
+elif [[ -n $1 ]] && [[ -e $1 ]]
 then
 	echo "You have entered a file path, taking a backup"
 	tar -zcvf backupfile.tar.gz $1
-elif [-z $1]
+elif [ -z $1 ]
 then
 	echo "Please enter any argument."
 else
