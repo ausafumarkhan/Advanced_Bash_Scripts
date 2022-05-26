24e3269b62a7cef9f1c4a29ec78d97281c91ad8e
ausafumarkhan <ausafumarkhan@gmail.com>
2022-05-27 02:56:21 +0530

    Script provide number of available network interface

---
+++ network_count.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+value=$(ip addr show | grep -v LOOPBACK | grep -ic mtu)
+
+if [ $value -eq 1]
+then
+	echo "Found a network Interface"
+elif [$value -gt 1]
+then
+	echo "Found multiple network interface"
+else
+	echo "No active network interface"
+fi
