b4d40899749e0b41dd1bca18ee9fcfd3ffdf5c05
ausafumarkhan <ausafumarkhan@gmail.com>
2024-04-27 00:58:30 +0000

    Add read and select command in the script

--- get_extensions.sh
+++ get_extensions.sh
@@ -12,12 +12,25 @@
 
 read -p "What is your first name?: " firstname
 read -p "What is your surname/family name?: " lastname
+PS3="What type of phone do you have?: "
+
+select  phone in headset handheld; do
+echo "Your phone type is $phone"
+break
+done
+
+PS3="What department do you work in? :"
+select department in finance sales "customer service" engineering; do
+echo "Your department is $department"
+break
+done 
+
 read -N 4 -p "What is your extension number? (must be 4 digits): " extension
 echo
-read -s -N 4 -p "What access code would you like to use when dialing in?: (must be 4 digits) " access
+read -N 4 -s -p "What access code would you like to use when dialing in?: (must be 4 digits) " access
 echo
 
-echo "$firstname,$lastname,$extension,$access" >> extension.csv
+echo "$firstname,$lastname,$phone,$department,$extension,$access" >> extension.csv
 
 exit 0
 
