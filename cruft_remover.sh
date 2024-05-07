832d1ffeef075aeb934c97067d8952a7fffa881d
ausafumarkhan <ausafumarkhan@gmail.com>
2024-05-07 07:06:41 +0000

    Modified cruft removal to delete unmodified files

--- cruft_remover.sh
+++ cruft_remover.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-read -r -p "Enter the number days: " num
+read -r -p "Enter the days of unmodified file: " num
 read -r -p "Enter the directory absolute path: " path
-readarray -t files < <(find "$path" -type f -mtime "$num") # it will delete the file which is n days
+readarray -t files < <(find "$path" -maxdepth 3 -type f -mtime "$num") # it will delete the file which is n days
 
 for file in "${files[@]}"; do
-	rm -r "$file"
+	rm -i "$file"
 	echo "removed $file"
 done
 
